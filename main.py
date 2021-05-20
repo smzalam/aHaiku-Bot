2d2a1b9fc5c9fb5a8fe49829df1c9b73072eed26
Syed Alam <alam.syedmz@gmail.com>
2021-05-20 09:47:49 -0400

    basic functions

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ from discord.ext import commands
 
 bot = commands.BOT(command_prefix="3")
 
+@bot.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(bot))
+
+@bot.listen('on_message')
+async def message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('3hello'):
+        await message.channel.send('Hello!')
+
