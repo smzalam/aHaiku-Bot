d0ad1ebc8ceba5561c2607d2aade3a4da7d247e4
Syed Alam <alam.syedmz@gmail.com>
2021-05-20 11:14:06 -0400

    add

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import mytoken as mt
+import syllables
 
 bot = commands.Bot(command_prefix="3")
 
@@ -15,5 +16,9 @@ async def message(message):
 
     if message.content.startswith('3hello'):
         await message.channel.send('Hello!')
+
+@bot.command()
+async def syl(ctx, *, s="word"):
+    await ctx.channel.send("The syllable count for '" + s + "' is " + str(syllables.estimate(s)))
 
 bot.run(mt.TOKEN)
